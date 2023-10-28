b96b5ef1bb3f7fede9195f5f072e2119b3731f65
Eric Zheng <eric.z1@outlook.com>
2023-10-27 22:42:09 -0400

    Completed after ADD SOME STYLE

--- lab5/project/schema.sql
+++ lab5/project/schema.sql
@@ -1,7 +1,7 @@
 drop table if exists entries;
 
-CREATE TABLE entries (
-  id integer primary key,
+create table entries (
+  id integer primary key autoincrement,
   title text not null,
-  text text not null,
+  text text not null
 );
